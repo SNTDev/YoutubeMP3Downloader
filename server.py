b29d2fb1c51caa816f89b42d39c06f12bbd9197a
MyeongJun <tkdlek5425@naver.com>
2016-10-20 14:23:32 +0900

    완성

--- server.py
+++ server.py
@@ -2,11 +2,20 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import session
+from flask import send_file
+from flask import Response, stream_with_context
+from flask import redirect
 from bs4 import BeautifulSoup
+from subprocess import call
+import StringIO
+import socket
 import sqlite3
 import requests
 import json
 import pafy
+import urllib
+import os
+import io
 
 app = Flask(__name__)
 
@@ -24,8 +33,30 @@ def get_file():
 
     video = pafy.new(url)
     best = video.getbest()
-    filename = best.download(filepath = './')
 
+    filename = best.download()
+
+    print(filename)
+
+    call(["mplayer", "-novideo", "-nocorrect-pts", "-ao", "pcm:waveheader", filename])
+    call(["lame", "-h", "-b", "192", "audiodump.wav",  filename + ".mp3"])
+    os.remove("audiodump.wav")
+    os.remove(filename)
+
+    print(filename)
+
+    #sound = AudioSegment.from_file(filename)
+
+    f = open(filename + ".mp3", "r")
+    sound_data = f.read()
+    strIO = StringIO.StringIO()
+    strIO.write(sound_data)
+    strIO.seek(0)
+    os.remove(filename + ".mp3")
+
+    #return redirect(best.url)
+    return send_file(strIO, attachment_filename= filename + ".mp3", as_attachment=True)
+    #return Response(stream_with_context(sound_data))
     return 'test'
 
 if __name__ == '__main__' :
